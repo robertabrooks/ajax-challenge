62d7c665c8d22a15e7a3316a572b89d3cef269f4
Robert Brooks <robertb1@uw.edu>
2014-11-18 10:45:30 -0800

    almost finished

--- js/app.js
+++ js/app.js
@@ -1,5 +1,76 @@
+
 "use strict";
-/*
-    app.js, main Angular application script
-    define your module and controllers here
-*/
+
+var commentsUrl = 'https://api.parse.com/1/classes/comments';
+
+angular.module('CommentApp', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = '99zLjvwSgJ5Yv0cQvzMo3MeOEufJnTEFMQU31B07';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'rVa5FYYUkCCfx9IDbYyrZHuW01oqAAup1vkOvaqf';
+    })
+    .controller('CommentController', function($scope, $http) {
+        $scope.loading = false;
+
+        $scope.refreshComments = function() {
+            $scope.loading = true;
+            $http.get(commentsUrl+'?order=-votes')
+                .success(function(data) {
+                    $scope.comments = data.results;
+                })
+                .error(function(err) {
+                    console.log(err);
+                })
+                .finally(function() {
+                    $scope.loading = false;
+                });
+        };
+
+        $scope.refreshComments();
+
+        $scope.newComment = {votes: 0};
+
+        $scope.incrementVotes = function(comment, amount) {
+            if (comment.votes > 0 || amount > 0) {
+                $http.put(commentsUrl + '/' + comment.objectId, {
+                    votes: {
+                        __op: 'Increment',
+                        amount: amount
+                    }
+                })
+                    .success(function (responseData) {
+                        console.log(responseData);
+                        comment.votes = responseData.votes;
+                    })
+                    .error(function () {
+                        console.log(err);
+                    })
+            }
+        };
+
+        $scope.addComment = function() {
+            $scope.loading = true;
+            $http.post(commentsUrl, $scope.newComment)
+                .success(function(responseData) {
+                    $scope.newComment.objectId = responseData.objectId;
+                    $scope.comments.push($scope.newComment);
+                    $scope.newComment = {votes: 0};
+                })
+                .error(function(err) {
+                    console.log(err);
+                })
+                .finally(function() {
+                    $scope.loading = false;
+                })
+        };
+
+        $scope.removeComment = function(comment) {
+            if (window.confirm("Are you sure you want to delete this comment?")) {
+                $scope.loading = true;
+                $http.delete(commentsUrl + '/' + comment.objectId)
+                    .finally(function () {
+                        $scope.refreshComments();
+                        $scope.loading = false;
+                    })
+            }
+        };
+    });
